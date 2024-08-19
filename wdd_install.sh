fdf923f00cd96496e0f6e206d92da7fac4637033
parallels <tobilasinde2012@yahoo.com>
2024-08-19 06:51:11 -0400

    updating install script

--- wdd_install.sh
+++ wdd_install.sh
@@ -5,7 +5,7 @@ if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     exit
 fi
 git clone https://github.com/tobilasinde/defacement-checker.git
-cd defacement-checker
-./install.sh
+# cd defacement-checker
+./defacement-checker/install.sh
 cd ..
 rm -r defacement-checker
